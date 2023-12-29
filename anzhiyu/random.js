5fa5e18d7b20be088b42f0049c7f006a030a37b4
ytstu <1875605680@qq.com>
2023-12-30 00:43:06 +0800

    Site updated: 2023-12-30 00:43:02

--- anzhiyu/random.js
+++
@@ -1,1 +0,0 @@
-var posts=["post/cfcee281.html","post/3cf822e1.html","post/c4e64a0d.html","post/9b1514a6.html","post/8523.html","post/9897d2d3.html","post/c70ee45d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
